cf615af563a0f54baaa0a727a39ad6adfb3d96d3
Ben Medcalf <ben.medcalf@gmail.com>
2015-06-09 22:31:33 -0400

    lots of cleanup to implement the global dictionary

--- gameredo.py
+++ gameredo.py
@@ -4,24 +4,21 @@ from sys import exit
 
 #now I will begin work on global_dicts
 
-<<<<<<< Updated upstream
-=======
 your_stats = {"hp":20,"ammo":20,"hp_scan":20}
 
->>>>>>> Stashed changes
 def dead(why):
 	print why
 	exit(0)
 
-def start(your_stats):
+def start(a):
 
 	print "---START STARTS HERE---"
 
 	print "You enter a dark, dingy room"
 	print "You stub your toe and look downward"
-	print "How convenient...a rifle...\n...and %d rounds" % your_ammo
+	print "How convenient...a rifle...\n...and %d rounds" % your_stats["ammo"]
 	print "You grab the old armor hanging on the wall"
-	print "Instructions read: Good for %d HP" % your_HP
+	print "Instructions read: Good for %d HP" % your_stats["hp"]
 	print "There are two doors"
 	print "Do you choose the Red Door\n...or the Blue door?"
 
@@ -44,7 +41,7 @@ def red_one(your_stats, monster_HP,monster_attack):
 
 	print "---RED_ONE STARTS HERE---"
 
-	print "You enter with %d HP, %d Ammo and %d HP_Scans" % (your_HP,your_ammo,HP_Scan)
+	print "You enter with %d HP, %d Ammo and %d HP scans" % (your_stats["hp"],your_stats["ammo"],your_stats["hp_scan"])
 	print "You point your rifle at the monster before you"
 
 	your_stats,monster_HP,monster_attack = shoot(your_stats,monster_HP,monster_attack)
@@ -57,7 +54,7 @@ def blue_one(your_stats, monster_HP,monster_attack):
 
 	print "---BLUE_ONE STARTS HERE---"
 
-	print "You enter with %d HP, %d Ammo and %d HP_Scans" % (your_HP,your_ammo,HP_Scan)
+	print "You enter with %d HP, %d Ammo and %d HP Scans" % (your_stats["hp"],your_stats["ammo"],your_stats["hp_scan"])
 	print "'Got dam smells like shit', you mutter"
 	print ""
 	print "You point your rifle at the weird, slimy before you"
@@ -73,25 +70,25 @@ def shoot(your_stats, monster_HP,monster_attack):
 
 	print "---shoot STARTS HERE---"
 
-	print "You have %d HP left" % your_HP
+	print "You have %d HP left" % your_stats["hp"]
 	print "How many shots do you fire?"
 
 	shots_fired = raw_input("> ")
 	shots_fired = int(shots_fired)
 
-	your_ammo = your_ammo - shots_fired
+	your_stats["ammo"] = your_stats["ammo"] - shots_fired
 	monster_HP = monster_HP - shots_fired
 
 	if monster_HP <= 0:
 		print "You killed it!"
-		print "You now have %d bullets left" % your_ammo
+		print "You now have %d bullets left" % your_stats["ammo"]
 		print "You finally look around the room"
 		print "There is a door going straight"
 		print "There is a door going left"
 		room_choose()
 
 	else:
-		print "you have %d bullets left" % your_ammo
+		print "you have %d bullets left" % your_stats["ammo"]
 		print "the monster has %d HP left" % monster_HP
 
 	return your_stats, monster_HP,monster_attack
@@ -100,17 +97,16 @@ def get_shot(your_stats,monster_HP,monster_attack):
 
 	print "---GET_SHOT STARTS HERE---"
 
-	your_HP = your_HP - monster_attack
+	your_stats["hp"] = your_stats["hp"] - monster_attack
 
-	if your_HP <= 0:
+	if your_stats["hp"] <= 0:
 		dead('you ran outta HP GOODBYE U DED')
 
 	else:
 		print "you take a hit, but survive!"
-		print "You have %d HP left" % your_HP
+		print "You have %d HP left" % your_stats["hp"]
 		return your_stats, monster_HP,monster_attack
 
-def room_choose(your_stats,monster_HP,monster_attack)
-
-
 start(your_stats)
+
+#def room_choose(your_stats,monster_HP,monster_attack):
